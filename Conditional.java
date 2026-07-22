b7e11302674e0c680fcab0101059486fa5da3706
Aakriti Singh <aakritis414@gmail.com>
2026-07-22 22:56:41 +0530

    Add files via upload

--- Conditional.java
+++ Conditional.java
@@ -2,14 +2,28 @@ import java.util.*;
 public class Conditional{
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
-     int age = sc.nextInt();
-     if (age>= 18)
-        {
-            System.out.println("You are an adult");
-        }
-        else
-        {
-            System.out.println("You are a minor");
-        }
+    //  int age = sc.nextInt();
+    //  if (age>= 18)
+    //     {
+    //         System.out.println("You are an adult");
+    //     }
+    //     else
+    //     {
+    //         System.out.println("You are a minor");
+    //     }
+    int button = sc.nextInt();
+    switch (button){
+        case 1:
+            System.out.println("Hello");
+            break;
+        case 2:
+            System.out.println("Namaste");
+            break;
+        case 3:
+            System.out.println("Bonjour");
+            break;
+        default:
+            System.out.println("Invalid button");
     }
+}
 }
