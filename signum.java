7a00ffa686f0a9ddd990b270ffd424526bf2275f
Aakriti Singh <aakritis414@gmail.com>
2026-07-23 21:53:41 +0530

    Add files via upload

---
+++ signum.java
@@ -0,0 +1,11 @@
+public import java.util.*;
+public class signum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        float n = sc.nextFloat();
+        System.out.println("The Greatest integer function of "+n+" is: "+Math.floor(n));
+    }
+} {
+    
+}
